ab803708267b6d48cbb08cc1bfd948d7b3a374e0
ms-masuma <masumashikder03@gmail.com>
2021-01-12 01:42:50 +0600

    home page pricing table added

--- js/app.js
+++ js/app.js
@@ -5,3 +5,22 @@ $('input,textarea').focus(function () {
 }).blur(function () {
     $(this).attr('placeholder', $(this).data('placeholder'));
 });
+
+
+// pricing carousel
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:25,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
